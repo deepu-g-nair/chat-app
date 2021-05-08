f15e3b935dd508cbf534b12a646a2b86119d4ca7
Deepu G Nair <deepukrishna811@gmail.com>
2021-05-08 21:46:03 +0530

    added login logic

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import React from "react";
 
 import "rsuite/dist/styles/rsuite-default.css";
 import "./styles/main.scss";
+import { BrowserRouter, Switch } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
 
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <PublicRoute path={"/signin"}>
+          <SignIn />
+        </PublicRoute>
+        <PrivateRoute>
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/misc/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+const config = {
+  apiKey: "AIzaSyCpHhJWp3uYnMQtiozY1o1Z8Wgd9RmT_AQ",
+  authDomain: "chat-web-app-875e4.firebaseapp.com",
+  projectId: "chat-web-app-875e4",
+  storageBucket: "chat-web-app-875e4.appspot.com",
+  messagingSenderId: "17455196600",
+  appId: "1:17455196600:web:6f78bc4474d3e07711c74d",
+};
+
+const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();

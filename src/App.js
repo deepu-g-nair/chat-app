dccf1242aebe406b1d5597a425601c21c038d0f1
Deepu G Nair <deepukrishna811@gmail.com>
2021-05-09 20:09:06 +0530

    added account linking and unlinking functionalities

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
+import { ProfileProvider } from "./context/profile.context";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <PublicRoute path={"/signin"}>
-          <SignIn />
-        </PublicRoute>
-        <PrivateRoute>
-          <Home />
-        </PrivateRoute>
-      </Switch>
-    </BrowserRouter>
+    <ProfileProvider>
+      <BrowserRouter>
+        <Switch>
+          <PublicRoute path="/signin">
+            <SignIn />
+          </PublicRoute>
+          <PrivateRoute path="/">
+            <Home />
+          </PrivateRoute>
+        </Switch>
+      </BrowserRouter>
+    </ProfileProvider>
   );
 };
 
---
+++ src/components/dashboard/ProviderBlock.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { Alert, Button, Icon, Tag } from "rsuite";
+import { auth } from "../../misc/firebase";
+import firebase from "firebase/app";
+
+const ProviderBlock = () => {
+  const [isConnected, setIsConnected] = useState({
+    "google.com": auth.currentUser.providerData.some(
+      (data) => data.providerId === "google.com"
+    ),
+    "facebook.com": auth.currentUser.providerData.some(
+      (data) => data.providerId === "facebook.com"
+    ),
+  });
+
+  const updateIsConnected = (providerId, value) => {
+    setIsConnected((p) => {
+      return {
+        ...p,
+        [providerId]: value,
+      };
+    });
+  };
+
+  const unlink = async (providerId) => {
+    try {
+      if (auth.currentUser.providerData.length === 1) {
+        throw new Error(`You cannot disconnect from ${providerId}`);
+      }
+      await auth.currentUser.unlink(providerId);
+      updateIsConnected(providerId, false);
+      Alert.info(`Disconnected from ${providerId}`, 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const unLinkFacebook = () => {
+    unlink("facebook.com");
+  };
+  const unLinkGoogle = () => {
+    unlink("google.com");
+  };
+
+  const link = async (provider) => {
+    try {
+      await auth.currentUser.linkWithPopup(provider);
+      Alert.info(`Linked with ${provider.providerId}`, 4000);
+      updateIsConnected(provider.providerId, true);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const linkFacebook = () => {
+    link(new firebase.auth.FacebookAuthProvider());
+  };
+  const linkGoogle = () => {
+    link(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <div>
+      {isConnected["google.com"] && (
+        <Tag closable color="green" onClose={unLinkGoogle}>
+          <Icon icon="google" /> Google
+        </Tag>
+      )}
+
+      {isConnected["facebook.com"] && (
+        <Tag closable color="blue" onClose={unLinkFacebook}>
+          <Icon icon="facebook" /> Facebook
+        </Tag>
+      )}
+
+      <div className="mt-2">
+        {!isConnected["google.com"] && (
+          <Button block color="green" onClick={linkGoogle}>
+            <Icon icon="google" />
+            Link to Google
+          </Button>
+        )}
+        {!isConnected["facebook.com"] && (
+          <Button block color="blue" onClick={linkFacebook}>
+            <Icon icon="facebook" />
+            Link to Facebook
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProviderBlock;

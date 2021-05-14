dfb7748d094ab971eb75f88d986074fa2cb4c60f
Deepu G Nair <deepukrishna811@gmail.com>
2021-05-14 11:20:28 +0530

    final commit - finished development

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "rsuite/dist/styles/rsuite-default.css";
 import "./styles/main.scss";
 import { BrowserRouter, Switch } from "react-router-dom";
 import SignIn from "./pages/SignIn";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
 import { ProfileProvider } from "./context/profile.context";
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -3,17 +3,24 @@ import { Alert, Button, Divider, Drawer } from "rsuite";
 import { useProfile } from "../../context/profile.context";
 import { database } from "../../misc/firebase";
 import EditableInput from "../EditableInput";
+import AvatarUploadBtn from "./AvatarUploadBtn";
 import ProviderBlock from "./ProviderBlock";
+import { getUserUpdates } from "../../misc/helpers";
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
   const onSave = async (newData) => {
-    const userNicknameRef = database
-      .ref(`/profiles/${profile.uid}`)
-      .child("name");
     try {
-      await userNicknameRef.set(newData);
+      const updates = await getUserUpdates(
+        profile.uid,
+        "name",
+        newData,
+        database
+      );
+
+      await database.ref().update(updates);
+
       Alert.success("Nickname is updated", 4000);
     } catch (err) {
       Alert.error(err.message, 4000);
@@ -35,6 +42,7 @@ const Dashboard = ({ onSignOut }) => {
           onSave={onSave}
           label={<h6 className="mb-2">Nickname</h6>}
         />
+        <AvatarUploadBtn />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
